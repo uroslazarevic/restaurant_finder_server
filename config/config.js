2563d54359be553843950b267316186b2e93b4e7
Uros <uros.lazarevic.la@gmail.com>
2019-07-28 16:33:58 +0200

    Change config settings for development

--- config/config.js
+++ config/config.js
@@ -26,10 +26,10 @@ module.exports = {
         dialect: 'postgres',
     },
     production: {
-        username: 'root',
-        password: null,
-        database: 'database_production',
-        host: '127.0.0.1',
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: 'https://resfinder-server.herokuapp.com/',
         dialect: 'postgres',
     },
 };
